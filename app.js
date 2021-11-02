ba8d8a0c792390b9b52b467844635192f84aa1d6
jleonen <jleonen2012@gmail.com>
2021-11-02 03:18:36 -0700

    Initial script

--- app.js
+++ app.js
@@ -1,1 +1,47 @@
 "use strict";
+
+const slider = function () {
+  const testimonials = document.querySelectorAll(".testimonial");
+  const btnLeft = document.querySelector(".btn--left");
+  const btnRight = document.querySelector(".btn--right");
+
+  let currentSlide = 0;
+  const maxSlides = testimonials.length;
+
+  const goToSlide = function (slide) {
+    testimonials.forEach(
+      (slide, index) =>
+        (slide.style.transform = `translateX(${100 * (index - slide)}%)`)
+    );
+  };
+
+  const nextSlide = function () {
+    if (currentSlide === maxSlides - 1) {
+      currentSlide = 0;
+    } else {
+      currentSlide++;
+    }
+
+    goToSlide(currentSlide);
+  };
+
+  const prevSlide = function () {
+    if (currentSlide === 0) {
+      currentSlide = maxSlides - 1;
+    } else {
+      currentSlide--;
+    }
+
+    goToSlide(currentSlide);
+  };
+
+  const init = function () {
+    goToSlide(0);
+  };
+  init();
+
+  btnRight.addEventListener("click", nextSlide);
+  btnLeft.addEventListener("click", prevSlide);
+};
+
+slider();
