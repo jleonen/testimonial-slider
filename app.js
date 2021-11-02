3da57e92e5218d0ea0fa486c4080daab07e15ed5
jleonen <jleonen2012@gmail.com>
2021-11-02 03:48:06 -0700

    Fixed function to go to next slide

--- app.js
+++ app.js
@@ -1,47 +1,51 @@
 "use strict";
 
-const slider = function () {
-  const testimonials = document.querySelectorAll(".testimonial");
-  const btnLeft = document.querySelector(".btn--left");
-  const btnRight = document.querySelector(".btn--right");
+const testimonials = document.querySelectorAll(".testimonial");
+const btnLeft = document.querySelector(".btn--left");
+const btnRight = document.querySelectorAll(".btn--right");
 
-  let currentSlide = 0;
-  const maxSlides = testimonials.length;
+let currentSlide = 0;
+const maxSlides = testimonials.length;
 
-  const goToSlide = function (slide) {
-    testimonials.forEach(
-      (slide, index) =>
-        (slide.style.transform = `translateX(${100 * (index - slide)}%)`)
-    );
-  };
-
-  const nextSlide = function () {
-    if (currentSlide === maxSlides - 1) {
-      currentSlide = 0;
-    } else {
-      currentSlide++;
-    }
-
-    goToSlide(currentSlide);
-  };
-
-  const prevSlide = function () {
-    if (currentSlide === 0) {
-      currentSlide = maxSlides - 1;
-    } else {
-      currentSlide--;
-    }
-
-    goToSlide(currentSlide);
-  };
-
-  const init = function () {
-    goToSlide(0);
-  };
-  init();
-
-  btnRight.addEventListener("click", nextSlide);
-  btnLeft.addEventListener("click", prevSlide);
+const goToSlide = function (slide) {
+  testimonials[slide].classList.remove("hidden");
 };
 
-slider();
+//   const goToSlide = function (slide) {
+//     testimonials.forEach(
+//       (slide, index) =>
+//         (slide.style.transform = `translateX(${100 * (index - slide)}%)`)
+//     );
+//   };
+
+const nextSlide = function () {
+  if (currentSlide + 1 === maxSlides) {
+    currentSlide = 0;
+  } else {
+    currentSlide++;
+  }
+
+  testimonials.forEach((item) => item.classList.add("hidden"));
+  goToSlide(currentSlide);
+};
+
+//   const prevSlide = function () {
+//     if (currentSlide === 0) {
+//       currentSlide = maxSlides - 1;
+//     } else {
+//       currentSlide--;
+//     }
+
+//     goToSlide(currentSlide);
+//   };
+
+// const init = function () {
+//   goToSlide(0);
+// };
+// init();
+btnRight.forEach((item, index) => item.addEventListener("click", nextSlide));
+
+// btnRight.addEventListener("click", nextSlide);
+//   btnLeft.addEventListener("click", prevSlide);
+
+goToSlide(0);
